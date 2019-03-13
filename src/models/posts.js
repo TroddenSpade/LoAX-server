7a218eabc94ad0142ccecb44604f6a42b2e6aa67
Parsa Sam <parsasam1378@gmail.com>
2019-03-14 01:30:06 +0330

    posts model added

---
+++ src/models/posts.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const postsSchema = mongoose.Schema({
+    address:{
+        type:String,
+        required:true,
+    },
+    geoHash:{
+        type:String,
+        required:true,
+    },
+    caption:{
+        type:String,
+        required:true,
+    },
+    region:{
+        latitude :{
+            type:Number,
+            required:true,
+        },
+        latitudeDelta:{
+            type:Number,
+            default: 0.0421
+        },
+        longitude:{
+            type:Number,
+            required:true,
+        },
+        longitudeDelta:{
+            type:Number,
+            default: 0.0421
+        }
+    },
+    image_url:{
+        type:String,
+        required:true,
+    },
+    tags:{
+        type:Array,
+    },
+
+    userid:{
+        type:String,
+        required:true,
+    }
+});
+
+const Posts = mongoose.model('Posts',postsSchema);
+
+module.exports = { Posts };
