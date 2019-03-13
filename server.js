7f43bd004518e7aa336e481f5774b1ab850406f6
Parsa Sam <parsasam1378@gmail.com>
2019-03-14 01:37:38 +0330

    models imported and POST user added

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 
+const {Users} = require('./src/models/users');
+const {Posts} = require('./src/models/posts');
+const {Reports} = require('./src/models/reports');
+
 const config = require('../config/config').get();
 const port = config.PORT ;
 
@@ -12,6 +16,18 @@ app.use(cookieParser());
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DATABASE);
+
+// POST
+app.post('/api/adduser',(req,res)=>{
+    const user = new Users(req.body);
+    user.save((err,doc)=>{
+        if(err) return res.status(400).send(err);
+        res.status(200).json({
+            post:true,
+            post_id:doc._id
+        })
+    })
+});
 
 app.listen(port,()=>{
     console.log("It's Working !")
