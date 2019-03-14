cd659012afb1c075300d6028abb1034c7b30444a
Parsa Sam <parsasam1378@gmail.com>
2019-03-14 13:01:56 +0330

    SECRET string added

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,8 @@
 const config = {
     SECRET : "",
     PORT:3000,
-    DATABASE : "mongodb://localhost:27017/loax"
+    DATABASE : "mongodb://localhost:27017/loax",
+    SECRET : "lakjsnf9awr9"
 }
 
 exports.get = function get(){
