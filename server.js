6d2043a9dac3be74c7910755423886239876a09d
troddenspade <parsasam1378@gmail.com>
2019-03-14 03:07:59 -0400

    GET POST UPDATE DELETE reqs added

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect(config.DATABASE,{
 		useNewUrlParser: true,
         useCreateIndex: true});
-        
+
 const {Users} = require('./src/models/users');
 const {Posts} = require('./src/models/posts');
 const {Reports} = require('./src/models/reports');
@@ -19,6 +19,25 @@ const {Reports} = require('./src/models/reports');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+// GET
+app.get('/api/getuser',(req,res)=>{
+    let id = req.query.id;
+    Users.findById(id,(err,doc)=>{
+        if(err) return res.status(400).send(err);
+        res.send(doc);
+    });
+});
+
+app.get('/api/posts',(req,res)=>{
+    let skip = parseInt(req.query.skip);
+    let limit = parseInt(req.query.limit);
+    let order = req.query.order;
+    Posts.find().skip(skip).sort({_id:order}).limit(limit).exec((err,doc)=>{
+        if(err) return res.status(400).send(err);
+        res.send(doc);
+    });
+});
 
 // POST
 app.post('/api/adduser',(req,res)=>{
@@ -31,6 +50,28 @@ app.post('/api/adduser',(req,res)=>{
             post_id:doc._id
         })
     })
+});
+
+// UPDATE
+app.post('/api/userupdate',(req,res)=>{
+    Users.findByIdAndUpdate(req.body._id,req.body,{new:true},(err,doc)=>{
+        if(err) return res.status(400).send(err);
+        res.json({
+            update:true,
+            user:doc,
+        });
+    });
+});
+
+// DELETE
+app.delete('/api/deletepost',(req,res)=>{
+    let id = req.query.id;
+    Posts.findByIdAndRemove(id,(err,doc)=>{
+        if(err) return res.status(400).send(err);
+        res.json({
+            delete:true,
+        });
+    });
 });
 
 app.listen(port,()=>{
