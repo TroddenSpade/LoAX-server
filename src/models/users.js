563125f6872674504e18f524670ae0411af75d5f
Parsa Sam <parsasam1378@gmail.com>
2019-03-14 13:02:58 +0330

    fields added to schema and functions added
    comparePassword,
    genToken,
    genToken funcs added

--- src/models/users.js
+++ src/models/users.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const config = require('./src/config/config').get();
+const SALT_I = 10;
 
 const usersSchema = mongoose.Schema({
     email:{
@@ -22,8 +24,57 @@ const usersSchema = mongoose.Schema({
         type:String,
         required:true,
         unique:1,
+    },
+    avatar:{
+        type:String,
+        default:"",
+    },
+    bio:{
+        type:String,
+    },
+    token:{
+        type:String,
     }
 });
+
+usersSchema.pre('save',(next)=>{
+    if(usersSchema.isModified('password')){
+        bcrypt.genSalt(SALT_I,(err,salt)=>{
+            if(err) return next(err);
+            bcrypt.hash(this.password,salt,(err,hash)=>{
+                if(err) return next(err);
+                this.password = hash;
+            });
+        });
+    }else{
+        next();
+    }
+});
+
+usersSchema.methods.comparePassword = (pass,cb)=>{
+    bcrypt.compare(pass,this.password,(err,isMatch)=>{
+        if(err) return cb(err);
+        cb(null,isMatch);
+    });
+};
+
+usersSchema.methods.genToken =(cb)=>{
+    const token = jwt.sign(this._id.toHexString(),config.SECRET);
+    this.token = token;
+    this.save((err,this)=>{
+        if(err) return cb(err);
+        cb(null,this);
+    });
+};
+
+usersSchema.statics.findByToken = (token,cb)=>{
+    jwt.verify(token,config.SECRET,(err,decode)=>{
+        this.findOne({'_id':decode,token:token},(err,user)=>{
+            if(err) return cb(err);
+            cb(null,user);
+        });
+    });
+}
 
 const Users = mongoose.model('Users',usersSchema);
 
