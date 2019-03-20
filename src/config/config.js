9f7d56257112baafe9c105387364f7945fb4eff9
troddenspade <parsasam1378@gmail.com>
2019-03-20 09:56:46 -0400

    vps address added

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,8 @@ const config = {
     SECRET : "",
     PORT:3000,
     DATABASE : "mongodb://localhost:27017/loax",
-    SECRET : "lakjsnf9awr9"
+    SECRET : "lakjsnf9awr9",
+    SERVER : "http://198.143.181.77:3000"
 }
 
 exports.get = function get(){
